5e83fc8f807ade57b139642d6bed3e0deb87b86f
stradomus <97191473+stradomus@users.noreply.github.com>
2022-01-15 20:18:44 +0100

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -48,7 +48,7 @@ subtitles = [sub1, sub2, sub3]
 SITESUBTITLE = choice(subtitles)
 
 ## Site images
-cubchoo = '/images/pokemon/cubchoo.gif width=100 height=100'
+cubchoo = '/images/pokemon/cubchoo.gif width=150 height=150'
 SITEIMAGE = cubchoo
 """
 siteimages = []
